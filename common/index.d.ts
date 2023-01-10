a40f99615e8c80df500c9628d7cff80a55cfd7bc
Leejjon <leejjon@gmail.com>
2023-01-10 14:29:58 +0100

    Forgot the decorator on the response body.

--- common/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class ResponseBody {
-    message: string;
-    constructor(message: string);
-}
-export declare class RequestBody {
-    name: string;
-    constructor(name: string);
-}
